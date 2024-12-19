e22a7d932ff91c7ea1007ae644e7b4ae3715aa98
Raul Almanza <ra.almanzaserrano@gmail.com>
2024-12-19 17:08:10 -0600

    Load a build producttion of react app

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import cors from 'cors';
 import authRoutes from './routes/auth';
 import eventRoutes from './routes/events';
 import { dbConnection } from './database/config';
+import path from 'path';
 
 // * create express server
 const app = expresss();
@@ -24,6 +25,10 @@ app.use(expresss.json());
 // * create a routes
 app.use('/api/auth', authRoutes);
 app.use('/api/events', eventRoutes);
+
+app.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
 // * start the server
 app.listen(process.env.PORT, () => {
